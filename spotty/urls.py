af7c37b5ef9dca90368899558abaf57520587669
awaciern <awaciern@ncsu.edu>
2019-07-18 23:09:57 -0400

    Hello World! up

--- spotty/urls.py
+++ spotty/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import include, url
+from appify import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    url('admin/', admin.site.urls, name='admin'),
+    url('^$', views.home, name='homepage')
 ]
